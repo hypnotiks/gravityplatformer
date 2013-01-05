dbf8f9c0d8dff5312fda4bd93ae4e426abb82340
unknown <24g@EEIGMPT.(none)>
2013-01-04 23:31:02 -0500

    Finished tutorial 4

--- src/game/character/Character.java
+++ src/game/character/Character.java
@@ -1,5 +1,10 @@
 package game.character;
  
+import java.util.HashMap;
+
+import game.enums.Facing;
+
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
  
@@ -7,13 +12,18 @@ public abstract class Character {
  
     protected float x;
     protected float y;
-    protected Image sprite;
+    protected Facing facing;
+    protected HashMap<Facing,Image> sprites;
+    protected HashMap<Facing,Animation> movingAnimations;
+    protected long                      lastTimeMoved;
  
     public Character(float x, float y) throws SlickException{
         this.x = x;
         this.y = y;
+        facing = Facing.RIGHT;
         //in case we forget to set the image, we don't want the game to crash, but it still has to be obvious that something was forgotten
-        sprite = new Image("data/img/placeholder_sprite.png");
+        setSprite(new Image("data/img/placeholder_sprite.png"));
+        
     }
  
     public float getX(){
@@ -25,7 +35,32 @@ public abstract class Character {
     }
  
     public void render(){
-        sprite.draw(x-2,y-2);
+    	//draw a moving animation if we have one and we moved within the last 150 miliseconds
+        if(movingAnimations != null && lastTimeMoved+150 > System.currentTimeMillis()){
+            movingAnimations.get(facing).draw(x-2,y-2);                
+        }else{            
+            sprites.get(facing).draw(x-2, y-2);          
+        }
+    }
+    
+    protected void setSprite(Image i){
+        sprites = new HashMap<Facing,Image>();
+        sprites.put(Facing.RIGHT, i);
+        sprites.put(Facing.LEFT , i.getFlippedCopy(true, false));
+    }
+    
+    protected void setMovingAnimation(Image[] images, int frameDuration){
+        movingAnimations = new HashMap<Facing,Animation>();
+ 
+        //we can just put the right facing in with the default images
+        movingAnimations.put(Facing.RIGHT, new Animation(images,frameDuration));
+ 
+        Animation facingLeftAnimation = new Animation();
+        for(Image i : images){
+            facingLeftAnimation.addFrame(i.getFlippedCopy(true, false), frameDuration);
+        }
+        movingAnimations.put(Facing.LEFT, facingLeftAnimation);
+ 
     }
  
 }
--- src/game/character/Player.java
+++ src/game/character/Player.java
@@ -1,5 +1,7 @@
 package game.character;
  
+import game.enums.Facing;
+
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
  
@@ -7,14 +9,24 @@ public class Player extends Character {
  
     public Player(float x, float y) throws SlickException{
         super(x,y);
-        sprite = new Image("data/img/characters/player/player.png");
+        setSprite(new Image("data/img/characters/player/player.png"));
+    
+        setMovingAnimation(new Image[]{new Image("data/img/characters/player/player_1.png"),new Image("data/img/characters/player/player_2.png"),
+                new Image("data/img/characters/player/player_3.png"),new Image("data/img/characters/player/player_4.png")}
+                ,125);
     }
     
     public void moveLeft(int delta){
         x = x - (0.15f*delta);
+        facing = Facing.LEFT;
+        lastTimeMoved = System.currentTimeMillis();
     }
  
     public void moveRight(int delta){
         x = x + (0.15f*delta);
+        facing = Facing.RIGHT;
+        lastTimeMoved = System.currentTimeMillis();
     }
+    
+    
 }
